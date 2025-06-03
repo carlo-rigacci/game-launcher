3ba8df677043ec60cc391632f4894da1431eb14f
Carlo Rigacci <72162133+carlo-rigacci@users.noreply.github.com>
2025-06-03 15:18:08 +0200

    init

---
+++ src/main/java/com/faintful/casino/launcher/GameLauncherApplication.java
@@ -0,0 +1,13 @@
+package com.faintful.casino.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLauncherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLauncherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faintful/casino/launcher/GameLauncherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faintful.casino.launcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLauncherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
